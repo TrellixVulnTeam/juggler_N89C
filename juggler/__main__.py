1d95a91cb5776d6b17d27e08442082415eb31ac2
Christian Meyer <konfuzzyus@gmail.com>
2014-02-03 21:04:16 +0100

    ! Change license to GPLv3

--- juggler/__main__.py
+++ juggler/__main__.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env/python
 
+"""
+    Juggler - Dirty dependency management and packaging for compiled code
+    Copyright (C) 2014  Christian Meyer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import argparse
 import config
 import dependency
@@ -9,7 +27,10 @@ import sys
 def create_argparser():
     parser = argparse.ArgumentParser(prog='juggler',
                                      version='v1.0',
-                                     description='')
+                                     description="""Juggler - Dirty dependency management and packaging for compiled code
+                                                    Copyright (C) 2014  Christian Meyer
+                                                    This program comes with ABSOLUTELY NO WARRANTY. This is free software,
+                                                    and you are welcome to redistribute it under certain conditions.""")
     
     parser.add_argument('COMMAND', action='store', choices=['fetch', 'publish'])
     parser.add_argument('PATH', action='store', help='Path to the project to be juggled. Must be a directory containing a juggle.xml')
