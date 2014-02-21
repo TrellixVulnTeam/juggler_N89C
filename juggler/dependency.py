d5fabb897b1fb2792c165c613f21563d2188153b
Christian Meyer <christian.meyer@scs.ch>
2014-02-21 18:48:37 +0100

    Refactor dependency code

--- juggler/dependency.py
+++ juggler/dependency.py
@@ -36,33 +36,25 @@ class DependencyManager:
                 self.__remote_listing.append(listing.load_remote_listing(repo))
             except listing.FileNotFound as error:
                 messages.UnableToAccessRemoteRepository(repo, error)
-    
+
     def deploy(self, required_packages, target_directory, ignore_local_builds, flavor):
         for package in required_packages:
-            source_info = self.find_best_source(package, ignore_local_builds, flavor)
-            if source_info is None:
-                raise RequiredPackageNotAvailable('None of the repositories known to me contain the required package %s with version %s' % (package['name'], package['version']))
-            target_file = None
-            messages.ResolvedPackage(package, flavor, source_info['package'])
-            if source_info['source_type'] == 'local':
-                target_file = os.path.join(source_info['package'].get_path(), source_info['package'].get_filename())
-            else:
-                filename = source_info['package'].get_filename()
-                source_url = '/'.join([source_info['package'].get_path(), filename])
-                target_file = os.path.join(self.__local_listing.get_root(), filename)
-                messages.DownloadingPackage(source_url)
-                urllib.urlretrieve(source_url, target_file)
-                self.__local_listing.add_package(source_info['package'].get_name(), str(source_info['package'].get_version()), source_info['package'].get_flavor())
-            # TODO only extract packages that are newer than the dependency in the build
+            source_info = self.resolve_source(ignore_local_builds, flavor, package)
+            archive_filename = self.fetch_archive(source_info)
             extract_dir = os.path.join(target_directory, package['name'])
-            if os.path.exists(extract_dir):
-                shutil.rmtree(extract_dir)
-            with tarfile.open(target_file, 'r') as archive:
-                archive.extractall(extract_dir)
-        # TODO create a file with include directives (for CMake)
+            self.extract_archive(archive_filename, extract_dir)
+
         # TODO remove unused dependencies from project dep folder
         self.__local_listing.store(self.__local_listing.get_root())
-    
+
+    def resolve_source(self, ignore_local_builds, flavor, package):
+        source_info = self.find_best_source(package, ignore_local_builds, flavor)
+        if source_info is None:
+            raise RequiredPackageNotAvailable('None of the repositories known to me contain the required package %s with version %s' % (package['name'], package['version']))
+        else:
+            messages.ResolvedPackage(package, flavor, source_info['package'])
+        return source_info
+
     def find_best_source(self, package, ignore_local_builds, flavor):
         best = {'package': self.__local_listing.get_package(package['name'], package['version'], ignore_local_builds, flavor),
                 'source': self.__local_listing,
@@ -78,4 +70,23 @@ class DependencyManager:
         if best['package'] is None:
             return None
         return best
-        
+
+    def fetch_archive(self, source_info):
+        target_file = None
+        if source_info['source_type'] == 'local':
+            target_file = os.path.join(source_info['package'].get_path(), source_info['package'].get_filename())
+        else:
+            filename = source_info['package'].get_filename()
+            source_url = '/'.join([source_info['package'].get_path(), filename])
+            target_file = os.path.join(self.__local_listing.get_root(), filename)
+            messages.DownloadingPackage(source_url)
+            urllib.urlretrieve(source_url, target_file)
+            self.__local_listing.add_package(source_info['package'].get_name(), str(source_info['package'].get_version()), source_info['package'].get_flavor())
+        return target_file
+
+    def extract_archive(self, archive_filename, extract_dir):
+        # TODO only extract packages that are newer than the dependency in the build
+        if os.path.exists(extract_dir):
+            shutil.rmtree(extract_dir)
+        with tarfile.open(archive_filename, 'r') as archive:
+            archive.extractall(extract_dir)
