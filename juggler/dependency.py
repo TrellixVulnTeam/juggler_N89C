1ede1bb0335c9aecc9bcd72702656fdbc8baf1cc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-14 21:05:07 +0000

    Adding tarfile member sanitization to extractall()

--- juggler/dependency.py
+++ juggler/dependency.py
@@ -89,4 +89,23 @@ class DependencyManager:
         if os.path.exists(extract_dir):
             shutil.rmtree(extract_dir)
         with tarfile.open(archive_filename, 'r') as archive:
-            archive.extractall(extract_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, extract_dir)
